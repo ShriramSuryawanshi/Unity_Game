da19a8c9a65f2f729fe42656978b807c9e4a35ae
ShriramSuryawanshi <shriram.a.suryawanshi@gmail.com>
2019-03-14 21:05:52 -0400

    Player done

--- Unity Game/Assets/Scripts/Player.cs
+++ Unity Game/Assets/Scripts/Player.cs
@@ -8,23 +8,35 @@ public class Player : MonoBehaviour
     private Rigidbody2D _rigid;
     private float _jumpForce = 5.0f;           
     private bool _resetJump = false;
-    private LayerMask _groundLayer;    
+    private LayerMask _groundLayer;
+    private bool _grounded = false;
+
     private float _speed = 2.5f;
 
     private PlayerAnimation _playerAnim;
     private SpriteRenderer _playerSprite;
+    private SpriteRenderer _swordArcSprite;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>();
         _playerAnim = GetComponent<PlayerAnimation>();
         _playerSprite = GetComponentInChildren<SpriteRenderer>();
+        _swordArcSprite = transform.GetChild(1).GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement();           
+        Movement();  
+        
+        if (Input.GetMouseButtonDown(0) && IsGrounded() == true)
+        {
+            _playerAnim.Attack();
+        }
     }
 
 
@@ -32,12 +44,15 @@ public class Player : MonoBehaviour
     {
         float move = Input.GetAxisRaw("Horizontal");
 
+        _grounded = IsGrounded();
+
         Flip(move);
         
         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true)
         {
             _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);            
             StartCoroutine(ResetJumpNeededRoutine());
+            _playerAnim.Jump(true);
         }
 
         _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
@@ -53,7 +68,10 @@ public class Player : MonoBehaviour
         if (hitInfo.collider != null)
         {
             if (_resetJump == false)
+            {
+                _playerAnim.Jump(false);
                 return true;
+            }
         }
 
         return false;
@@ -64,11 +82,21 @@ public class Player : MonoBehaviour
     {
         if (move > 0)
         {
-            _playerSprite.flipX = false;
+            _playerSprite.flipX = false;            
+            _swordArcSprite.flipY = false;
+
+            Vector3 newPos = _swordArcSprite.transform.localPosition;
+            newPos.x = 1.01f;
+            _swordArcSprite.transform.localPosition = newPos;
         }
         else if (move < 0)
         {
-            _playerSprite.flipX = true;
+            _playerSprite.flipX = true;            
+            _swordArcSprite.flipY = true;
+
+            Vector3 newPos = _swordArcSprite.transform.localPosition;
+            newPos.x = -1.01f;
+            _swordArcSprite.transform.localPosition = newPos;
         }
     }
 
--- Unity Game/Assets/Scripts/PlayerAnimation.cs
+++ Unity Game/Assets/Scripts/PlayerAnimation.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour    
 {
-
     private Animator _anim;
-
-
+    private Animator _swordAnimation;
+     
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         _anim = GetComponentInChildren<Animator>();
+        _swordAnimation = transform.GetChild(1).GetComponent<Animator>();
     }
 
-
+  
     public void Move(float move)
     {
         _anim.SetFloat("Move", Mathf.Abs(move));
+    }
+
+
+    public void Jump(bool jumping)
+    {
+        _anim.SetBool("Jumping", jumping);
+    }
+
+
+    public void Attack()
+    {
+        _anim.SetTrigger("Attack");
+        _swordAnimation.SetTrigger("SwordAnimation");
     }
 }
